fdaaa37e22e15bd7677f420e1ef9123c8cfc3a58
Asimov <napo123@rediffmail.com>
2021-02-06 13:09:29 +0530

    google map working

--- app/src/main/java/com/gtropy/MapActivity.java
+++ app/src/main/java/com/gtropy/MapActivity.java
@@ -1,9 +1,12 @@
 package com.gtropy;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -33,9 +36,24 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     @Override
     public void onMapReady(GoogleMap gmap){
         this.map=gmap;
+
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        map.setMyLocationEnabled(true);
+
         Intent inten=getIntent();
         lati=inten.getDoubleExtra("lattitude",0.0);
         longi=inten.getDoubleExtra("longitude",0.0);
+        //lati=39.916668;
+         //longi=116.383331;
         LatLng abc= new LatLng(lati,longi);
         map.addMarker(new MarkerOptions().position(abc).title("Googlemapra"));
         map.moveCamera(CameraUpdateFactory.newLatLng(abc));
